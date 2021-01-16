e132836a2c9ec64b341a0864c2d148b75366d82e
oopanpan <oopanpan@gmail.com>
2021-01-16 06:16:06 -0800

    mom_i_did_it

---
+++ lib/artist.rb
@@ -0,0 +1,28 @@
+require 'pry'
+class Artist
+
+    @@all =[]
+
+    attr_accessor :name, :songs
+
+    def initialize(name)
+        @name = name
+    end
+
+    def songs
+        Song.all.select {|song| song.artist == self}
+    end
+
+    def add_song(song)
+        song.artist = self
+    end
+
+    def add_song_by_name(song_name)
+        self.add_song(Song.new(song_name))
+    end
+
+    def self.song_count
+        
+        Song.all.count
+    end
+end
